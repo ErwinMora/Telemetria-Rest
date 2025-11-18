fbbc0d91eb2bcf978dedbdc0159fb487eb5068f8
Erwin Mora <erwinmm17@outlook.com>
2025-11-18 13:04:57 -0600

    Cambio de archivo index.js y varibales de entorno

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ app.use("/api/telemetria", telemetriaRoute);
 
 const PORT = process.env.PORT || 3000;;
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Servidor corriendo en http://172.29.120.3:${PORT}`);
+    // console.log(`Servidor corriendo en http://172.29.120.3:${PORT}`);
+    console.log("Conexión establecida correctamente!");
+    
 });
